b154a8a132ad717cdd6633199c0b5c9365f7588e
hamzaODprogramer <hamzaouadoud116@gmail.com>
2024-11-15 10:10:44 +0100

    add_template

--- src/pages/vistorSide/landing.jsx
+++ src/pages/vistorSide/landing.jsx
@@ -1,5 +1,53 @@
+import React from 'react';
+
+import About from '../../components/landing/About';
+import Analytics from '../../components/landing/Analytics';
+import Canvas from '../../components/landing/Canvas';
+import Features from '../../components/landing/Features';
+import Header from '../../components/landing/Header';
+import LazyShow from '../../components/landing/LazyShow';
+import MainHero from '../../components/landing/MainHero';
+import MainHeroImage from '../../components/landing/MainHeroImage';
+import Pricing from '../../components/landing/Pricing';
+import Product from '../../components/landing/Product';
+
 export default function Landing(){
-    return <>
-        <h1 className="text-xl font-bold">Landing Page</h1>
-    </>
-}
+  return (
+    <div className={`bg-background grid gap-y-16 overflow-hidden`}>
+      <div className={`relative bg-background`}>
+        <div className="max-w-7xl mx-auto">
+          <div
+            className={`relative z-10 pb-8 bg-background sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32`}
+          >
+            <Header />
+            <MainHero />
+          </div>
+        </div>
+        <MainHeroImage />
+      </div>
+      <Canvas />
+      <LazyShow>
+        <>
+          <Product />
+          <Canvas />
+        </>
+      </LazyShow>
+      <LazyShow>
+        <>
+          <Features />
+          <Canvas />
+        </>
+      </LazyShow>
+      <LazyShow>
+        <Pricing />
+      </LazyShow>
+      <LazyShow>
+        <>
+          <Canvas />
+          <About />
+        </>
+      </LazyShow>
+      <Analytics />
+    </div>
+  );
+};
--- src/pages/vistorSide/layoute.jsx
+++ src/pages/vistorSide/layoute.jsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout(){
     return <>
-        <h1>Nav</h1>
+
             <Outlet/>
-        <h1>Footer</h1>
+
     </>
 }
