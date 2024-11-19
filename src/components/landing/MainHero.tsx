585d68b06fb3258fd863a0ccb4004ad62cb6edbe
Laaliji <zakariaelaalijihh2003@gmail.com>
2024-11-19 21:06:38 +0100

    resolved merging MainHero

--- src/components/landing/MainHero.tsx
+++ src/components/landing/MainHero.tsx
@@ -17,7 +17,6 @@ const MainHero = () => {
     <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
       <div className="sm:text-center lg:text-left">
         <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-<<<<<<< HEAD
           <span className="block xl:inline">
             <div className="h-40">
               <Typewriter
@@ -29,7 +28,6 @@ const MainHero = () => {
                 }}
               />
             </div>
-=======
           <span className="block xl:inline">Créez un site web sans coder, </span>{' '}
           <span className={`block text-primary xl:inline`}>
             <Typewriter
@@ -41,7 +39,6 @@ const MainHero = () => {
               deleteSpeed={50}
               delaySpeed={1000}
             />
->>>>>>> 7005623a57a6de2ce381711ad2788fdace2a42d1
           </span>
         </h1>
         <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
