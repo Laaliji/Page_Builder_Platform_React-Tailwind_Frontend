5d8d565e45de63dc09236e65856f9518912cc8b1
Hnioua Abdessamad <abdessamadhnioua@gmail.com>
2024-11-27 01:37:05 +0000

    1er page dans sprint Admin

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
-import Layout from './pages/vistorSide/layoute.jsx'
-import Landing from './pages/vistorSide/landing.jsx'
-import Editor from './pages/panelSide/editor.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./pages/vistorSide/layoute.jsx";
+import LayoutAdmin from "./pages/admin/layoute.jsx";
+import Landing from "./pages/vistorSide/landing.jsx";
+import Editor from "./pages/panelSide/editor.jsx";
+import Admin from "./pages/admin/dashboard.jsx";
 
 const router = createBrowserRouter([
-  { 
-    path : '/' , 
-    element : <Layout/> , 
-    children: [
-      { path: '/' , element : <Landing/> },
-      {},
-      {}
-    ] 
+  {
+    path: "/",
+    element: <Layout />,
+    children: [{ path: "/", element: <Landing /> }, {}, {}],
   },
-  { path : '/editor' , element : <Editor /> }
-]) 
+  { path: "/editor", element: <Editor /> },
+  {
+    path: "/",
+    element: <LayoutAdmin />,
+    children: [{ path: "/admin", element: <Admin /> }, {}, {}],
+  },
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
---
+++ src/pages/admin/dashboard.jsx
@@ -0,0 +1,16 @@
+import Layout from "./layoute";
+
+export function Admin() {
+  return (
+    <Layout>
+      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+        <div className="aspect-video rounded-xl bg-muted/50" />
+        <div className="aspect-video rounded-xl bg-muted/50" />
+        <div className="aspect-video rounded-xl bg-muted/50" />
+      </div>
+      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+    </Layout>
+  );
+}
+
+export default Admin;
